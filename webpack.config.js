d2c3c43ef4eb62993c3891d5de199af20a4048b3
Artem Lipatov <a.lipatov@2gis.ru>
2016-07-09 23:12:13 +0300

    update production assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-let path = require('path');
-let webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: NODE_ENV === 'production' ? null : 'source-map',
   entry: [
     './src/main'
   ],
   output: {
-    path: path.join(__dirname, 'public/assets'),
+    path: path.join(__dirname, 'html/assets'),
     filename: 'bundle.js',
-    publicPath: '/public/assets/'
+    publicPath: '/html/assets/'
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+          warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    })
   ],
   module: {
     loaders: [{
